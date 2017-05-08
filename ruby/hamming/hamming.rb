93178da057e858cd7543fd9c125ec188b1bc2398
Justin Garavel <xxjeep@gmail.com>
2017-05-08 15:32:37 +0900

    Swapped enumeration type in hamming.rb. Fetched new exercise 'gigasecond'

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,14 +5,10 @@ end
 class Hamming
   def self.compute(strand1, strand2)
     unless strand1.size == strand2.size
-      raise ArgumentError, 'First strand is longer than second' if strand1.size > strand2.size
-      raise ArgumentError, 'Second strand is longer than first' if strand1.size < strand2.size
+      raise ArgumentError, "strand1.size != strand2.size (#{strand1.size} != #{strand2.size})"
     end
-
-    hamming_distance = 0
-    strand1.size.times do |index|
-      hamming_distance += 1 unless strand1[index] == strand2[index]
+    (0..(strand1.size)).count do |index|
+      strand1[index] != strand2[index]
     end
-    hamming_distance
   end
 end
