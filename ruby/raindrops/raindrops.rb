39cea9385476ebff3f778237df0c27a4824d384f
Justin Garavel <xxjeep@gmail.com>
2017-05-09 09:09:08 +0900

    Fetched raindrops assignment and set up gameplan

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,22 @@
+module BookKeeping
+  VERSION = 3
+end
+
+
+class Raindrops
+
+  def self.convert(number)
+    #find the factors of number
+
+
+    #iterate through these factors to see if they map to our factors that provide an output
+
+
+    #output the strings for these factors
+
+
+    #return string
+
+
+  end
+end
