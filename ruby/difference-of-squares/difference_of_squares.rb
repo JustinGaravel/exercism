d8d051a072abe14c7a3718e7937d2bec710530cc
Justin Garavel <xxjeep@gmail.com>
2017-05-15 15:16:46 +0900

    Cleaned up code a bit and increase readability by changing numeration method

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -6,23 +6,20 @@ class Squares
 
   def initialize(val)
     @val = val
-    #puts "@val is set to #{@val}"
   end
 
   def square_of_sum
     sum = 0
-    @val.times do |i|
-      #puts "Adding #{i} to #{sum}"
-      sum += (i+1)
+    1.upto(@val) do |i|
+      sum += (i)
     end
     sum**2
   end
 
   def sum_of_squares
     sum = 0
-    @val.times do |i|
-      #puts "Adding #{i} to #{sum}"
-      sum += ((i+1)**2)
+    1.upto(@val) do |i|
+      sum += ((i)**2)
     end
     sum
   end
@@ -30,5 +27,5 @@ class Squares
   def difference
     square_of_sum - sum_of_squares
   end
-  
+
 end
