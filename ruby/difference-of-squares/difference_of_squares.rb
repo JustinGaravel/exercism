6fa54903990af47fba9d886420461c662d01af83
Justin Garavel <xxjeep@gmail.com>
2017-05-15 15:14:01 +0900

    Code is ugly but difference_of_squares is passing all tests

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -4,17 +4,31 @@ end
 
 class Squares
 
-  def initialize
-
+  def initialize(val)
+    @val = val
+    #puts "@val is set to #{@val}"
   end
-
-  def new
-
-  end
-
-  
 
   def square_of_sum
-    #nothing here to see
+    sum = 0
+    @val.times do |i|
+      #puts "Adding #{i} to #{sum}"
+      sum += (i+1)
+    end
+    sum**2
   end
+
+  def sum_of_squares
+    sum = 0
+    @val.times do |i|
+      #puts "Adding #{i} to #{sum}"
+      sum += ((i+1)**2)
+    end
+    sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+  
 end
