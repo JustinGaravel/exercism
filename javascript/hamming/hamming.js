c6f3eb9d6851afd4f3c941d60738d399f2c9fa10
Justin Garavel <xxjeep@gmail.com>
2017-06-25 17:49:55 +0900

    working hamming project

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,17 +1,16 @@
 
 // Constructor
 function Hamming() {
-  var this.ham1 = [];
-  var this.ham2 = [];
-  var this.hammingDistance = 0;
+
 };
 
 Hamming.prototype.compute = function(input1, input2) {
   this.ham1 = input1.split("");
   this.ham2 = input2.split("");
+  this.hammingDistance = 0;
 
   if ((this.ham1.length) != (this.ham2.length)) {
-    throw "DNA strands must be of equal length.";
+    throw Error("DNA strands must be of equal length.");
   } else {
     for (i = 0; i < this.ham1.length; i ++) {
       if (this.ham1[i] != this.ham2[i]) {
@@ -19,4 +18,7 @@ Hamming.prototype.compute = function(input1, input2) {
       }
     }
   };
+  return this.hammingDistance;
 };
+
+module.exports = Hamming;
